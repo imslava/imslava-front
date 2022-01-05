e1fe083b0437d190f78605deb344a6f4faca205d
Slava Khripushin <slava.cpa@yandex.ru>
2022-01-05 14:13:30 +0300

    upd main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,52 @@
-console.log('hi')
+/*
+ *
+ *
+ * MAIN
+ *
+ *
+ */
+
+$(function () {
+
+	init()
+
+	$('[data-fancybox]').fancybox({
+		touch: false,
+		autoFocus: false,
+		backFocus: false,
+		closeExisting: true
+	})
+
+})
+
+/*
+ *
+ *
+ * FUNCTION
+ *
+ *
+ */
+
+const init = () => {
+	maskPhone()
+}
+
+const maskPhone = () => {
+
+	let maskedElements = [],
+		el = document.querySelectorAll('.masked')
+
+	if (el.length > 0) {
+		const mask = {
+			mask: '+7 (000) 000-00-00'
+		}
+		el.forEach(item => {
+			maskedElements.push(new IMask(item, mask))
+		});
+	}
+
+	$('.masked').click(function () {
+		if ($(this).val() == '') $(this).val('+7 ')
+	})
+
+}
